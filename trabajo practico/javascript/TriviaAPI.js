59c908b673846c954c8f5896e6eefad3ba04106a
Bruno González <brunobrunito048@gmail.com>
2026-05-05 15:13:34 -0300

    Add files via upload

---
+++ trabajo practico/javascript/TriviaAPI.js
@@ -0,0 +1,23 @@
+class TriviaAPI {
+    constructor() {
+    }
+    async getPreguntas(cantidad = 10, categoria = "", dificultad = "") {
+
+        const endpoint = "https://opentdb.com/api.php?amount=10&type=multiple";
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        console.log(data.results);
+
+
+
+    }
+    async getCategorias() {
+        const endpoint = "https://opentdb.com/api_category.php";
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        console.log(data.trivia_categories);
+    }
+
+}
+
+export default TriviaAPI;
