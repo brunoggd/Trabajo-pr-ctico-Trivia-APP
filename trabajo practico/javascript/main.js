b5a1455e30d7e7a23592e447d03cd59621a54941
Bruno González <brunobrunito048@gmail.com>
2026-05-04 21:49:02 -0300

    Add files via upload

---
+++ trabajo practico/javascript/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    // const contenedor_el_del_emdio = document.querySelector('#1');
+    
+    function decodificarHTML(texto) {
+        contenedor_el_del_emdio.createElement('textarea');
+        // contenedor_el_del_emdio.innerHTML = texto;
+        // return contenedor_el_del_emdio.value;
+    }
+});
