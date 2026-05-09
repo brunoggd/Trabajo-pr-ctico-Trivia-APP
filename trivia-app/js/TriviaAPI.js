a8b03502fe6fdcc3cab61c7493eaf0bb7cdc3835
Martin <barasmartinezequiel@gmail.com>
2026-05-08 21:00:48 -0300

    Add files via upload

--- trivia-app/js/TriviaAPI.js
+++ trivia-app/js/TriviaAPI.js
@@ -4,10 +4,13 @@ class TriviaAPI {
     constructor() {
     }
     async getPreguntas(cantidad = 10, categoria = "", dificultad = "") {
-
-        const endpoint = "https://opentdb.com/api.php?amount=10&type=multiple";
+        
+        const endpoint = `https://opentdb.com/api.php?amount=${cantidad}&category=${categoria}&difficulty=${dificultad}&type=multiple`;
         const response = await fetch(endpoint);
         const data = await response.json();
+
+        console.log(data.results);
+
         return data.results;
 
     }
--- trivia-app/js/TriviaGAME.js
+++ trivia-app/js/TriviaGAME.js
@@ -5,17 +5,63 @@ class TriviaGAME {
         this.preguntas = [];
         this.preguntaActual = 0;
         this.puntaje = 0;
+
+        this.aumento = 1;
     }
 
-    async iniciar(preguntas) {
-        const estado = 0;
+    iniciar(preguntas) {
+        this.preguntas = preguntas;
+
+        this.preguntaActual = 0;
+        this.puntaje = 0;
     }
 
-    async responder(respuesta) {
+    getPreguntaActual() {
+        return this.preguntas[this.preguntaActual];
     }
 
-    async haTerminado() {
+    siguiente() {
+        this.preguntaActual ++;
+    }
 
+    responder(respuesta, contenedor) {
+        if(respuesta === this.getPreguntaActual().correct_answer) {
+            this.puntaje ++;
+            this.siguiente();
+            
+            this.actualizarcontenedor(contenedor);
+
+            // console.log("LA RESPUESTA ES CORRECTA!!!", "INDICE:", this.preguntaActual);
+            return true;
+        }
+
+        else {
+            this.siguiente();
+
+            this.actualizarcontenedor(contenedor);
+
+            // console.log("SOS UN MOGOLICO", "INDICE:", this.preguntaActual);
+            return false;
+        }
+    }
+
+    actualizarcontenedor (contenedor) {
+        if (this.preguntaActual < this.preguntas.length) {
+            contenedor.innerHTML = `<div id="contenedor-preguntaindex-score" class="wrapper margin-auto small-gap">
+                                    <h2 class="question-title">Pregunta ${this.preguntaActual + this.aumento} de 10</h2>
+                                    <h2 class="question-title">Puntuación: ${this.puntaje}</h2>
+                                </div>`;
+        }
+    }
+
+    haTerminado() {
+        if(this.preguntaActual >= this.preguntas.length) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
     }
 }
 
